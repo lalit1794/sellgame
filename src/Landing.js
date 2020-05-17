f9d6d336bd7e4af2e5fe719b0c6fcc14e12bd267
lalit <lalit1794@gmail.com>
2020-05-18 00:05:27 +0530

    changes for form

--- src/Landing.js
+++ src/Landing.js
@@ -2,18 +2,22 @@ import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
+import { Formik } from "formik";
+import { ReactComponent as CheckboxIcon } from "feather-icons/dist/icons/arrow-right.svg";
 
-import Header, { NavLink, NavLinks, PrimaryLink as PrimaryLinkBase, LogoLink, NavToggle, DesktopNavLinks } from "./headers/light.js";
 
-const StyledHeader = styled(Header)`
-  ${tw`pt-8 max-w-none w-full`}
-  ${DesktopNavLinks} ${NavLink}, ${LogoLink} {
-    ${tw`text-gray-100 hover:border-gray-300 hover:text-gray-300`}
-  }
-  ${NavToggle}.closed {
-    ${tw`text-gray-100 hover:text-pink-500`}
-  }
-`;
+
+import Header, { NavLinks } from "./headers/light.js";
+
+// const StyledHeader = styled(Header)`
+//   ${tw`pt-8 max-w-none w-full`}
+//   ${DesktopNavLinks} ${NavLink}, ${LogoLink} {
+//     ${tw`text-gray-100 hover:border-gray-300 hover:text-gray-300`}
+//   }
+//   ${NavToggle}.closed {
+//     ${tw`text-gray-100 hover:text-pink-500`}
+//   }
+// `;
 
 const Actions = styled.div`
   ${tw`relative max-w-lg text-center mx-auto lg:mx-0`}
@@ -25,7 +29,7 @@ const Actions = styled.div`
   }
 `;
 
-const PrimaryLink = tw(PrimaryLinkBase)`rounded-full`
+// const PrimaryLink = tw(PrimaryLinkBase)`rounded-full`
 const Container = styled.div`
   ${tw`relative -mx-8 -mt-8 bg-center bg-cover h-screen min-h-screen`}
   background-image: url("https://i.ibb.co/TvKCphd/banner-without-layover.png");
@@ -33,8 +37,10 @@ const Container = styled.div`
 
 const OpacityOverlay = tw.div`z-10 absolute inset-0 bg-black opacity-75`;
 const Description = tw.p`text-gray-500 font-medium text-xl max-w-lg mt-2 mb-4 sm:mt-1 text-center sm:text-left`
-const HeroContainer = tw.div`z-20 relative px-6 sm:px-8 mx-auto h-full flex flex-col`;
+const HeroContainer = tw.div`z-20 relative px-6 sm:px-8 mx-auto h-full flex flex-col overflow-x-hidden`;
 const Content = tw.div`px-4 flex flex-1 flex-col justify-center items-center`;
+const FeatureIcon = tw(CheckboxIcon)`w-5 h-5 text-indigo-500 inline`;
+
 
 const Heading = styled.h1`
   ${tw`text-3xl text-center sm:text-4xl lg:text-5xl xl:text-6xl font-black text-gray-100 leading-snug -mt-24 sm:mt-0`}
@@ -43,7 +49,8 @@ const Heading = styled.h1`
   }
 `;
 
-const PrimaryAction = tw.button`rounded-full px-8 py-3 mt-10 text-sm sm:text-base sm:mt-16 sm:px-8 sm:py-4 bg-gray-100 font-bold shadow transition duration-300 bg-pink-500 text-gray-100 hocus:bg-pink-700 hocus:text-gray-200 focus:outline-none focus:shadow-outline`;
+
+// const PrimaryAction = tw.button`rounded-full px-8 py-3 mt-10 text-sm sm:text-base sm:mt-16 sm:px-8 sm:py-4 bg-gray-100 font-bold shadow transition duration-300 bg-pink-500 text-gray-100 hocus:bg-pink-700 hocus:text-gray-200 focus:outline-none focus:shadow-outline`;
 
 export default () => {
   const navLinks = [
@@ -64,10 +71,53 @@ export default () => {
           <Heading>
               Buy Game Accounts
           </Heading>
-          <Description>Select Game | Select Account | Buy Account</Description>
-          <Actions>
-            <input type="text" placeholder="Your E-mail Address" />
-            <button>Notify Me!!</button>
+          <Description>Select Game <FeatureIcon /> Select Account <FeatureIcon /> Buy Account</Description>
+          <Actions >
+          <Formik
+            initialValues={{ email: "" }}
+            onSubmit={async values => {
+              const requestOptions = {
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({ title: 'React POST Request Example', email: values.email })
+              };
+              const response = await fetch('https://app.99inbound.com/api/e/XiJNjIXz', requestOptions);
+              const data = await response.json();
+              alert("Cool!! We'll let you know pretty soon.");
+            }}
+          >
+            {props => {
+              const {
+                values,
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit
+              } = props;
+              return (
+                <form onSubmit={handleSubmit}>
+                  <input
+                    id="email"
+                    placeholder="Enter your email"
+                    type="text"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                  )}
+
+                  <button type="submit" disabled={isSubmitting}>
+                    Submit
+                  </button>
+
+                </form>
+              );
+            }}
+          </Formik>
           </Actions>
         </Content>
       </HeroContainer>
