58f0f084a6cea68f2100a9fdd071933a0a32bc28
lalit <lalit1794@gmail.com>
2020-05-20 00:44:43 +0530

    Updates

---
+++ precache-manifest.305d0d9b1fb139b9ce1c5e7470d8c529.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d892ad256c813ba07d57c7aebca5bbe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7faac10b1289994bf278",
+    "url": "/static/css/2.13c6718b.chunk.css"
+  },
+  {
+    "revision": "768918543f262ae07b13",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7faac10b1289994bf278",
+    "url": "/static/js/2.80a3e07c.chunk.js"
+  },
+  {
+    "revision": "f925ff808c907f1e05af05a547934faf",
+    "url": "/static/js/2.80a3e07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768918543f262ae07b13",
+    "url": "/static/js/main.d9a0aa5b.chunk.js"
+  },
+  {
+    "revision": "d048aec46536c656f54a",
+    "url": "/static/js/runtime-main.0cdada11.js"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sellgame/precache-manifest.82ea2917d44ef2a267e4948990ce0a66.js"
+  "/precache-manifest.305d0d9b1fb139b9ce1c5e7470d8c529.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sellgame/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
